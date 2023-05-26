1e74b05e08e3d328f9bbcedadbc18bfea32c0741
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-05-26 16:09:35 +0700

    membuat coid doublelinkedlist

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -10,7 +10,7 @@ public:
 	Node* prev;
 };
 
-class DoubleLinkList {
+class DoubleLinkedList {
 private:
 	Node* START = NULL;
 public :
@@ -22,6 +22,53 @@ public :
 	void revtraverse();
 	void hapus();
 	void searchData();
-
-
 };
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nama;
+	cout << "\nEnter the roll number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of student: ";
+	cin >> nama;
+
+	Node* newNode = new Node(); //step 1
+	newNode->noMhs = nim; //step 2
+	newNode->nama = nama; //step 2
+
+	/*insert a node in the beginig of a doubly - linked list*/
+	if (START == NULL || nim <= START->noMhs) {// check if data null
+		if (START != NULL && nim == START->noMhs) {
+			cout << "\nDuplicate number not allowed" << endl;
+			return;
+		}
+		newNode->next = START; //step 3
+		if (START != NULL)
+			START->prev = newNode; // step4
+		newNode->prev = NULL; //Step 5
+		START = newNode; // step 6
+		return;
+	}
+	
+	/*Inserting a Node Between Two Nodes in the list*/
+	Node* current = START; //1.a
+	Node* previous = NULL; //1.b 
+	while (current->next != NULL && current->next->noMhs < nim) //step 1c
+	{
+		previous = current; //1.d
+		current = current->next; //1.e
+	}
+
+	if (current->next != NULL && nim == current->next->noMhs) {
+		cout << "\nDuplicate roll number not allowed" <<endl;
+		return;
+	}
+
+	newNode->next = current->next; //step 4
+	newNode->prev = current; //step5
+	if (current->next != NULL)
+		current->next->prev = newNode; //step 6
+	current->next = newNode; // step 7
+}
+
+bool DoubleLinkedList 
